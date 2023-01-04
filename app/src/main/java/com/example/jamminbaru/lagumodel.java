ec17601decc8311c42abfa481ea942d97f1b7119
Elroi Yoshua <elroiyoshua@gmail.com>
2023-01-04 21:14:45 +0700

    Making RecyclerView functional

---
+++ app/src/main/java/com/example/jamminbaru/lagumodel.java
@@ -0,0 +1,29 @@
+package com.example.jamminbaru;
+
+public class lagumodel {
+    String judul,band,url;
+    int image;
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public lagumodel(String judul, String band, int image, String url) {
+        this.judul = judul;
+        this.band = band;
+        this.image = image;
+        this.url = url;
+    }
+}
