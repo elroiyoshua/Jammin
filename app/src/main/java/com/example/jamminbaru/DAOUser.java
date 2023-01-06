9765e1f3a8a0559606711b7714ddbf31808f7fba
fallennnn <fallensky86dxp@gmail.com>
2023-01-06 10:17:16 +0700

    Update: - Creator Login - Creator Register - Creator Home - Creator Profile - Peletakkan beberapa Class ke dalam Package yang Berbeda
    ~ 06-01-2023

--- app/src/main/java/com/example/jamminbaru/DAOUser.java
+++ app/src/main/java/com/example/jamminbaru/DAOUser.java
@@ -5,14 +5,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class DAOUser {
-//    private DatabaseReference databaseReference;
-//
-//    public DAOUser(){
-//        FirebaseDatabase db = FirebaseDatabase.getInstance();
-//        databaseReference = db.getReference(User.class.getSimpleName());
-//    }
-//
-//    public Task<Void> add(User usr){
-//        return databaseReference.push().setValue(usr);
-//    }
+    private DatabaseReference databaseReference;
+
+    public DAOUser(){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        databaseReference = db.getReference(User.class.getSimpleName());
+    }
+
+    public Task<Void> add(User usr){
+        return databaseReference.push().setValue(usr);
+    }
 }
